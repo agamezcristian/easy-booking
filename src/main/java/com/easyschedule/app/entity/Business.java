97ed11f3d0623a9caf98220ae6e75e4890e2e3af
Cristian Agamez <cagamez@bancolombia.com.co>
2025-06-17 20:58:40 -0500

    add adjusment entity and liquibase business

--- src/main/java/com/easyschedule/app/entity/Business.java
+++ src/main/java/com/easyschedule/app/entity/Business.java
@@ -1,7 +1,14 @@
 package com.easyschedule.app.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 
 @NoArgsConstructor
@@ -10,15 +17,27 @@ import lombok.*;
 @Setter
 @Builder
 @Entity
-@Table(name = "business")
+@Table(name = "business", indexes = {
+        @Index(name = "idx_business_id", columnList = "id"),
+        @Index(name = "idx_business_email", columnList = "email"),
+        @Index(name = "idx_business_name", columnList = "name"),
+        @Index(name = "idx_business_status", columnList = "status")
+})
 public class Business extends EasyBookingBaseEntity {
 
+    @Column(name = "name", nullable = false)
     private String name;
+    @Column(name = "description", nullable = false, columnDefinition = "TEXT")
     private String description;
+    @Column(name = "phone", nullable = false)
     private String phone;
+    @Column(name = "email", unique = true, nullable = false)
     private String email;
-    private String status;
+    @Column(name = "status")
+    private Boolean status = true;
+    @Column(name = "country", nullable = false)
     private String country;
+    @Column(name = "city", nullable = false)
     private String city;
     private String state;
 
